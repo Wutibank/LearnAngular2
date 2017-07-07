7ed7cb3fd50dd7ade72a223a24a5e4048c621da7
wutibank <wutihot@hotmail.com>
2017-07-07 18:40:54 +0700

    Router&Navigate

--- myApp1/src/app/product/product.component.ts
+++ myApp1/src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -8,13 +9,15 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ProductComponent implements OnInit {
 params : string;
-  constructor(private route : ActivatedRoute) { 
+  constructor(private route : ActivatedRoute,private router : Router) { 
     this.params = route.snapshot.params['id'];
-
-
   }
 
   ngOnInit() {
+
+  }
+  gotoContact(){
+    this.router.navigate(['contact']);
   }
 
 }
